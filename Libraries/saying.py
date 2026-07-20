d929ce3661e6de155fb5eb840c914678bbc41ad4
Tawheed Ahmed Ansari <tawheedansari4@gmail.com>
2026-07-20 17:44:44 +0530

    CS 50 Libraries

---
+++ Libraries/saying.py
@@ -0,0 +1,14 @@
+def main():
+    hello("Tawheed")
+    goodbye("Tawheed")
+
+
+def hello(name):
+    print(f"Hello {name}")
+
+def goodbye(name):
+    print(f"Goodbye {name}")
+
+
+if __name__ == "__main__":
+    main()
