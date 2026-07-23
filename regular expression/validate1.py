e73005d3441ae6b093c94f201bc3213e3b6b3074
Tawheed Ahmed Ansari <tawheedansari4@gmail.com>
2026-07-23 17:12:20 +0530

    Practiced regular expression with built in functions with example

--- regular expression/validate1.py
+++ regular expression/validate1.py
@@ -9,14 +9,22 @@
     ?  -> 0 or 1 repetition
     {m}  -> m repetititons
     {m,n}  -> m-n repetitions
+    
+    ^  -> matches the start of the string
+    $  -> matches the end of the string or just before the newline at the end of the string
+    
+    []. -> set of characters
+    [^]  -> complementing the set
 '''
 
 import re 
 
-email = input("What is ur email ? ")
+email = input("What is ur email ? ").strip()
 
 #if re.search("..*@..*", email):
-if re.search(r".+@.+\.edu", email):
+#if re.search(r".+@.+\.edu", email):
+#if re.search(r"^.+@.+\.edu$", email):
+if re.search(r"^[^@]+@[^@]+\.edu$", email):
     print("valid")
 else:
-    print("invalid")
+    print("invalid")    
