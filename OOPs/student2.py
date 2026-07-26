93898021b724d17a944fd85ae2645e97476e353a
Tawheed Ahmed Ansari <tawheedansari4@gmail.com>
2026-07-26 12:38:06 +0530

    Created my own function named charm and called it using the Harry Potter Reference

--- OOPs/student2.py
+++ OOPs/student2.py
@@ -1,18 +1,31 @@
 class Student():
-    def __init__(self, name, house):
+    def __init__(self, name, house, petronus):
         if not name:
             raise ValueError("Name Missing")
         if house not in ["Griffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid House ")
         self.name = name
         self.house = house
+        self.petronus = petronus
     
     def __str__(self):          # __str__ -> Automatically gets passed 
         return f"{self.name} is from {self.house}"
+    
+    def charm(self):
+        match self.petronus:
+            case "Stag":
+                return "🐎"
+            case "Otter":
+                return "🦦"
+            case "Jack Russell terrier":
+                return "🐶"
+            case _ :
+                return "🪄"
 
 def main():
     student = get_student()
-    print(student)
+    print("Expecto Patronum! ")
+    print(student.charm())
     
 def get_student():
     '''student = Student()
@@ -20,7 +33,8 @@ def get_student():
     student.house = input("house ")'''
     name = input("Name ")
     house = input("House ")
-    return Student(name, house)
+    petronus = input("Peetronus: ")
+    return Student(name, house,petronus)
     
 
 if __name__ == "__main__":
