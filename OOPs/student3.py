99d2ed96e7a96c37ac440498d88e9b88e1b73c1c
Tawheed Ahmed Ansari <tawheedansari4@gmail.com>
2026-07-26 18:46:41 +0530

    Introduction and how to use Getter and Setter

---
+++ OOPs/student3.py
@@ -0,0 +1,39 @@
+class Student():
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Name Missing")
+        if house not in ["Griffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid House ")
+        self.name = name
+        self.house = house
+    
+    def __str__(self):          
+        return f"{self.name} is from {self.house}"
+    
+    @property # Getter -> function or a clas which gets a certain attribute or variable 
+    def house(self):
+        return self.house 
+    
+    @house.setter # Setter -> function that sets some value
+    def house(self, house):
+        if house not in ["Griffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid House")
+        self.house = house
+    
+def main():
+    student = get_student()
+    student.house = "Number four, Privet Drive"
+    print(student)
+    
+def get_student():
+    '''student = Student()
+    student.name = input("name ")
+    student.house = input("house ")'''
+    name = input("Name ")
+    house = input("House ")
+    return Student(name, house)
+    
+
+if __name__ == "__main__":
+    main()
+    
