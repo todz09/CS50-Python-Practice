fe7194a4cbcaa9cf0810694a126953b8ca47bebe
Tawheed Ahmed Ansari <tawheedansari4@gmail.com>
2026-08-09 05:29:37 +0530

    using '**' for unpacking a dictionary

--- Extra/unpack.py
+++ Extra/unpack.py
@@ -1,6 +1,7 @@
 def total(galleons, sickles, knuts):
     return (galleons * 493 * 29 + sickles) * 29 + knuts
 
-coins = [100, 50, 25]
+#coins = [100, 50, 25]
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 
-print(total(*coins), "knuts")
+print(total(**coins), "knuts")
